943847d8613f16b32757886957c51f16719a538a
jamesorwell <james.m.orwell@gmail.com>
2016-11-14 13:57:08 +0000

    moved musicServiceFactory to source packages

--- MusicService/src/main/java/ku/piii/music/MusicServiceFactory.java
+++ MusicService/src/main/java/ku/piii/music/MusicServiceFactory.java
@@ -4,16 +4,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ku.piii.marshalling.JacksonJSONMarshallingSupport;
 import ku.piii.mp3.MP3PathToMusicMapperImpl;
-import ku.piii.music.MusicRepositoryImpl;
-import ku.piii.music.MusicService;
-import ku.piii.music.MusicServiceImpl;
 import ku.piii.nio.file.TextFileStoreImpl;
 
 public class MusicServiceFactory {
-	public static MusicService getMusicServiceInstance() {
-		final MusicService musicService = new MusicServiceImpl(
-				new MusicRepositoryImpl(new JacksonJSONMarshallingSupport(new ObjectMapper()), new TextFileStoreImpl()),
-				new MP3PathToMusicMapperImpl());
-		return musicService;
-	}
+
+    private static MusicService instance = null;
+    private static Object mutex = new Object();
+
+    private MusicServiceFactory() {
+    }
+
+    public static MusicService getMusicServiceInstance() {
+        if (instance == null) {
+            synchronized (mutex) {
+                if (instance == null) {
+                    instance = new MusicServiceImpl(
+                            new MusicRepositoryImpl(new JacksonJSONMarshallingSupport(
+                                    new ObjectMapper()),
+                                    new TextFileStoreImpl()),
+                            new MP3PathToMusicMapperImpl()
+                    );
+                }
+            }
+
+        }
+        return instance;
+    }
 }
